ca7fe3b1967b945aea0185b4e5359975ec2df5f9
jakob <krause.jakobmichael@gmail.com>
2021-11-30 18:05:28 +0100

    made it pritty

--- bayes/stan_file.hpp
+++ bayes/stan_file.hpp
@@ -17,20 +17,22 @@ using namespace stan::math;
 
 
 stan::math::profile_map profiles__;
-static constexpr std::array<const char*, 15> locations_array__ =
+static constexpr std::array<const char*, 15> locations_array__ = 
 {" (found before start of program)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 8, column 3 to column 10)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 9, column 3 to column 10)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 8, column 1 to column 8)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 9, column 1 to column 8)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 18, column 1 to column 55)",
  " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 13, column 1 to column 32)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 14, column 3 to column 21)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 15, column 3 to column 21)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 14, column 1 to column 19)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 15, column 1 to column 19)",
  " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 2, column 1 to column 16)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 3, column 10 to column 11)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 3, column 3 to column 15)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 4, column 10 to column 11)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 4, column 3 to column 15)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 3, column 8 to column 9)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 3, column 1 to column 13)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 4, column 8 to column 9)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 4, column 1 to column 13)",
  " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 5, column 8 to column 9)",
- " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 5, column 1 to column 14)"};
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 5, column 1 to column 14)",
+ " (in '/home/jakob/Documents/Studium/master_thesis/bayes/stan_file.stan', line 18, column 7 to column 8)"};
 
 
 
@@ -40,27 +42,27 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
   int n;
   Eigen::Matrix<double, -1, 1> x__;
   Eigen::Matrix<double, -1, 1> y__;
-  Eigen::Matrix<double, -1, 1> dy__;
+  Eigen::Matrix<double, -1, 1> dy__; 
   Eigen::Map<Eigen::Matrix<double, -1, 1>> x{nullptr, 0};
   Eigen::Map<Eigen::Matrix<double, -1, 1>> y{nullptr, 0};
   Eigen::Map<Eigen::Matrix<double, -1, 1>> dy{nullptr, 0};
-
+ 
  public:
   ~stan_file_model() { }
-
+  
   inline std::string model_name() const final { return "stan_file_model"; }
 
   inline std::vector<std::string> model_compile_info() const noexcept {
     return std::vector<std::string>{"stanc_version = stanc3 v2.28.1", "stancflags = "};
   }
-
-
+  
+  
   stan_file_model(stan::io::var_context& context__,
                   unsigned int random_seed__ = 0,
                   std::ostream* pstream__ = nullptr) : model_base_crtp(0) {
     int current_statement__ = 0;
     using local_scalar_t__ = double ;
-    boost::ecuyer1988 base_rng__ =
+    boost::ecuyer1988 base_rng__ = 
         stan::services::util::create_rng(random_seed__, 0);
     (void) base_rng__;  // suppress unused var warning
     static constexpr const char* function__ = "stan_file_model_namespace::stan_file_model";
@@ -70,13 +72,13 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
     try {
       int pos__;
       pos__ = std::numeric_limits<int>::min();
-
+      
       pos__ = 1;
       current_statement__ = 7;
       context__.validate_dims("data initialization","n","int",
            std::vector<size_t>{});
       n = std::numeric_limits<int>::min();
-
+      
       current_statement__ = 7;
       n = context__.vals_i("n")[(1 - 1)];
       current_statement__ = 7;
@@ -88,7 +90,7 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
            std::vector<size_t>{static_cast<size_t>(n)});
       x__ = Eigen::Matrix<double, -1, 1>(n);
       new (&x) Eigen::Map<Eigen::Matrix<double, -1, 1>>(x__.data(), n);
-
+      
       {
         std::vector<local_scalar_t__> x_flat__;
         current_statement__ = 9;
@@ -111,7 +113,7 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
            std::vector<size_t>{static_cast<size_t>(n)});
       y__ = Eigen::Matrix<double, -1, 1>(n);
       new (&y) Eigen::Map<Eigen::Matrix<double, -1, 1>>(y__.data(), n);
-
+      
       {
         std::vector<local_scalar_t__> y_flat__;
         current_statement__ = 11;
@@ -134,7 +136,7 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
            std::vector<size_t>{static_cast<size_t>(n)});
       dy__ = Eigen::Matrix<double, -1, 1>(n);
       new (&dy) Eigen::Map<Eigen::Matrix<double, -1, 1>>(dy__.data(), n);
-
+      
       {
         std::vector<local_scalar_t__> dy_flat__;
         current_statement__ = 13;
@@ -156,12 +158,12 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
       stan::lang::rethrow_located(e, locations_array__[current_statement__]);
     }
     num_params_r__ = 1 + 1;
-
+    
   }
-
-  template <bool propto__, bool jacobian__ , typename VecR, typename VecI,
-  stan::require_vector_like_t<VecR>* = nullptr,
-  stan::require_vector_like_vt<std::is_integral, VecI>* = nullptr>
+  
+  template <bool propto__, bool jacobian__ , typename VecR, typename VecI, 
+  stan::require_vector_like_t<VecR>* = nullptr, 
+  stan::require_vector_like_vt<std::is_integral, VecI>* = nullptr> 
   inline stan::scalar_type_t<VecR> log_prob_impl(VecR& params_r__,
                                                  VecI& params_i__,
                                                  std::ostream* pstream__ = nullptr) const {
@@ -175,16 +177,16 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
     (void) DUMMY_VAR__;  // suppress unused var warning
     static constexpr const char* function__ = "stan_file_model_namespace::log_prob";
     (void) function__;  // suppress unused var warning
-
+    
     try {
       local_scalar_t__ a;
       a = DUMMY_VAR__;
-
+      
       current_statement__ = 1;
       a = in__.template read<local_scalar_t__>();
       local_scalar_t__ b;
       b = DUMMY_VAR__;
-
+      
       current_statement__ = 2;
       b = in__.template read<local_scalar_t__>();
       {
@@ -202,12 +204,12 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
     }
     lp_accum__.add(lp__);
     return lp_accum__.sum();
-    } // log_prob_impl()
-
-  template <typename RNG, typename VecR, typename VecI, typename VecVar,
-  stan::require_vector_like_vt<std::is_floating_point, VecR>* = nullptr,
-  stan::require_vector_like_vt<std::is_integral, VecI>* = nullptr,
-  stan::require_std_vector_vt<std::is_floating_point, VecVar>* = nullptr>
+    } // log_prob_impl() 
+    
+  template <typename RNG, typename VecR, typename VecI, typename VecVar, 
+  stan::require_vector_like_vt<std::is_floating_point, VecR>* = nullptr, 
+  stan::require_vector_like_vt<std::is_integral, VecI>* = nullptr, 
+  stan::require_std_vector_vt<std::is_floating_point, VecVar>* = nullptr> 
   inline void write_array_impl(RNG& base_rng__, VecR& params_r__,
                                VecI& params_i__, VecVar& vars__,
                                const bool emit_transformed_parameters__ = true,
@@ -220,23 +222,23 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
     (void) propto__;
     double lp__ = 0.0;
     (void) lp__;  // dummy to suppress unused var warning
-    int current_statement__ = 0;
+    int current_statement__ = 0; 
     stan::math::accumulator<double> lp_accum__;
     local_scalar_t__ DUMMY_VAR__(std::numeric_limits<double>::quiet_NaN());
     constexpr bool jacobian__ = false;
     (void) DUMMY_VAR__;  // suppress unused var warning
     static constexpr const char* function__ = "stan_file_model_namespace::write_array";
     (void) function__;  // suppress unused var warning
-
+    
     try {
       double a;
       a = std::numeric_limits<double>::quiet_NaN();
-
+      
       current_statement__ = 1;
       a = in__.template read<local_scalar_t__>();
       double b;
       b = std::numeric_limits<double>::quiet_NaN();
-
+      
       current_statement__ = 2;
       b = in__.template read<local_scalar_t__>();
       out__.write(a);
@@ -244,14 +246,14 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
       if (logical_negation((primitive_value(emit_transformed_parameters__) ||
             primitive_value(emit_generated_quantities__)))) {
         return ;
-      }
+      } 
       if (logical_negation(emit_generated_quantities__)) {
         return ;
-      }
+      } 
       std::vector<double> y_tilde;
       y_tilde = std::vector<double>(n, std::numeric_limits<double>::quiet_NaN());
-
-
+      
+      
       current_statement__ = 3;
       assign(y_tilde,
         normal_rng(add(b, multiply(a, stan::math::cos(x))), dy, base_rng__),
@@ -260,11 +262,11 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
     } catch (const std::exception& e) {
       stan::lang::rethrow_located(e, locations_array__[current_statement__]);
     }
-    } // write_array_impl()
-
-  template <typename VecVar, typename VecI,
-  stan::require_std_vector_t<VecVar>* = nullptr,
-  stan::require_vector_like_vt<std::is_integral, VecI>* = nullptr>
+    } // write_array_impl() 
+    
+  template <typename VecVar, typename VecI, 
+  stan::require_std_vector_t<VecVar>* = nullptr, 
+  stan::require_vector_like_vt<std::is_integral, VecI>* = nullptr> 
   inline void transform_inits_impl(VecVar& params_r__, VecI& params_i__,
                                    VecVar& vars__,
                                    std::ostream* pstream__ = nullptr) const {
@@ -273,97 +275,97 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
     stan::io::serializer<local_scalar_t__> out__(vars__);
     int current_statement__ = 0;
     local_scalar_t__ DUMMY_VAR__(std::numeric_limits<double>::quiet_NaN());
-
+    
     try {
       int pos__;
       pos__ = std::numeric_limits<int>::min();
-
+      
       pos__ = 1;
       local_scalar_t__ a;
       a = DUMMY_VAR__;
-
+      
       a = in__.read<local_scalar_t__>();
       out__.write(a);
       local_scalar_t__ b;
       b = DUMMY_VAR__;
-
+      
       b = in__.read<local_scalar_t__>();
       out__.write(b);
     } catch (const std::exception& e) {
       stan::lang::rethrow_located(e, locations_array__[current_statement__]);
     }
-    } // transform_inits_impl()
-
+    } // transform_inits_impl() 
+    
   inline void get_param_names(std::vector<std::string>& names__) const {
-
+    
     names__ = std::vector<std::string>{"a", "b", "y_tilde"};
-
-    } // get_param_names()
-
+    
+    } // get_param_names() 
+    
   inline void get_dims(std::vector<std::vector<size_t>>& dimss__) const {
-
+    
     dimss__ = std::vector<std::vector<size_t>>{std::vector<size_t>{},
       std::vector<size_t>{}, std::vector<size_t>{static_cast<size_t>(n)}};
-
-    } // get_dims()
-
+    
+    } // get_dims() 
+    
   inline void constrained_param_names(
                                       std::vector<std::string>& param_names__,
                                       bool emit_transformed_parameters__ = true,
                                       bool emit_generated_quantities__ = true) const
     final {
-
+    
     param_names__.emplace_back(std::string() + "a");
     param_names__.emplace_back(std::string() + "b");
     if (emit_transformed_parameters__) {
-
+      
     }
-
+    
     if (emit_generated_quantities__) {
       for (int sym1__ = 1; sym1__ <= n; ++sym1__) {
         {
           param_names__.emplace_back(std::string() + "y_tilde" + '.' + std::to_string(sym1__));
-        }
+        } 
       }
     }
-
-    } // constrained_param_names()
-
+    
+    } // constrained_param_names() 
+    
   inline void unconstrained_param_names(
                                         std::vector<std::string>& param_names__,
                                         bool emit_transformed_parameters__ = true,
                                         bool emit_generated_quantities__ = true) const
     final {
-
+    
     param_names__.emplace_back(std::string() + "a");
     param_names__.emplace_back(std::string() + "b");
     if (emit_transformed_parameters__) {
-
+      
     }
-
+    
     if (emit_generated_quantities__) {
       for (int sym1__ = 1; sym1__ <= n; ++sym1__) {
         {
           param_names__.emplace_back(std::string() + "y_tilde" + '.' + std::to_string(sym1__));
-        }
+        } 
       }
     }
-
-    } // unconstrained_param_names()
-
+    
+    } // unconstrained_param_names() 
+    
   inline std::string get_constrained_sizedtypes() const {
-
+    
     return std::string("[{\"name\":\"a\",\"type\":{\"name\":\"real\"},\"block\":\"parameters\"},{\"name\":\"b\",\"type\":{\"name\":\"real\"},\"block\":\"parameters\"},{\"name\":\"y_tilde\",\"type\":{\"name\":\"array\",\"length\":" + std::to_string(n) + ",\"element_type\":{\"name\":\"real\"}},\"block\":\"generated_quantities\"}]");
-
-    } // get_constrained_sizedtypes()
-
+    
+    } // get_constrained_sizedtypes() 
+    
   inline std::string get_unconstrained_sizedtypes() const {
-
+    
     return std::string("[{\"name\":\"a\",\"type\":{\"name\":\"real\"},\"block\":\"parameters\"},{\"name\":\"b\",\"type\":{\"name\":\"real\"},\"block\":\"parameters\"},{\"name\":\"y_tilde\",\"type\":{\"name\":\"array\",\"length\":" + std::to_string(n) + ",\"element_type\":{\"name\":\"real\"}},\"block\":\"generated_quantities\"}]");
-
-    } // get_unconstrained_sizedtypes()
-
-
+    
+    } // get_unconstrained_sizedtypes() 
+    
+  
     // Begin method overload boilerplate
     template <typename RNG>
     inline void write_array(RNG& base_rng,
@@ -372,7 +374,7 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
                             const bool emit_transformed_parameters = true,
                             const bool emit_generated_quantities = true,
                             std::ostream* pstream = nullptr) const {
-      const size_t num_params__ =
+      const size_t num_params__ = 
   (1 + 1);
       const size_t num_transformed = 0;
       const size_t num_gen_quantities = n;
@@ -393,7 +395,7 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
                             bool emit_transformed_parameters = true,
                             bool emit_generated_quantities = true,
                             std::ostream* pstream = nullptr) const {
-      const size_t num_params__ =
+      const size_t num_params__ = 
   (1 + 1);
       const size_t num_transformed = 0;
       const size_t num_gen_quantities = n;
@@ -436,7 +438,7 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
       const std::array<Eigen::Index, 2> constrain_param_sizes__{1, 1};
       const auto num_constrained_params__ = std::accumulate(
         constrain_param_sizes__.begin(), constrain_param_sizes__.end(), 0);
-
+    
      std::vector<double> params_r_flat__(num_constrained_params__);
      Eigen::Index size_iter__ = 0;
      Eigen::Index flat_iter__ = 0;
@@ -450,8 +452,8 @@ class stan_file_model final : public model_base_crtp<stan_file_model> {
      }
      vars.resize(num_params_r__);
      transform_inits_impl(params_r_flat__, params_i, vars, pstream__);
-    } // transform_inits()
-
+    } // transform_inits() 
+    
 };
 }
 using stan_model = stan_file_model_namespace::stan_file_model;
@@ -472,3 +474,5 @@ stan::math::profile_map& get_stan_profile_data() {
 }
 
 #endif
+
+
--- bayes/stan_file.stan
+++ bayes/stan_file.stan
@@ -1,19 +1,19 @@
 data {
 	int<lower=0> n;
-  	vector[n] x;
-  	vector[n] y;
+	vector[n] x;
+	vector[n] y;
 	vector[n] dy;
 }
 parameters {
-  	real a;
-  	real b;
+	real a;
+	real b;
 }
 
 model {
 	y ~ normal(b + a * cos(x), dy);
-  	a ~ normal(0,100);
-  	b ~ normal(0,100);
+	a ~ normal(0,100);
+	b ~ normal(0,100);
 }
 generated quantities {
-   array[n] real y_tilde = normal_rng(b + a * cos(x),dy);
+	array[n] real y_tilde = normal_rng(b + a * cos(x),dy);
 }
