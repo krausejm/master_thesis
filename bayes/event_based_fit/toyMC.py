da6f2412d25eedfa0739aab9ecc900df66660197
Jakob Krause <krause.jakobmichael@gmail.com>
2022-03-25 13:47:00 +0100

    create toy data, talk

---
+++ bayes/event_based_fit/toyMC.py
@@ -0,0 +1,85 @@
+import nest_asyncio
+nest_asyncio.apply()
+import cmdstanpy as sp
+import numpy as np
+from scipy.optimize import curve_fit
+import pandas as pd
+import matplotlib.pyplot as plt
+from cycler import cycler
+import matplotlib.patches as mpatches
+import seaborn as sns
+import arviz as az
+import scipy.stats as stats
+import ROOT as r
+import time
+import arviz as az
+
+plt.rcParams["xtick.minor.visible"] =  True
+plt.rcParams["ytick.minor.visible"] =  True
+plt.rcParams["mathtext.fontset"]="cm"
+plt.rcParams['errorbar.capsize'] = 3
+plt.rcParams['mathtext.rm'] = 'serif'
+plt.rcParams['font.family']='serif'
+plt.rcParams['font.size']=22
+plt.rcParams['axes.grid'] = True
+plt.rcParams['grid.linestyle']=':'
+plt.rcParams['grid.color']='black'
+plt.rcParams['axes.prop_cycle'] = cycler(color=['black', 'red', 'blue', 'green'])
+
+
+def fit(nsamples,nbins):
+    cols=[f'toybin{i:02d}' for i in range(nbins)]
+    diagnostics_df=pd.DataFrame(columns=cols,index=['sigma_median','mcse','rhat'])
+    sigma_df=pd.DataFrame(columns=cols)
+    for i in range(nbins):#no. of toy bins
+        #read data
+        df=pd.read_csv(f"toybins/toybin{i:02d}.txt",sep="\t")
+        #df=pd.read_csv(f"new_toy_MC.txt",sep="\t")
+        df.columns=['pol','phi','weight']
+        #these are prompt peak events
+        prmpt=df.loc[df['weight']==1]
+        prmpt=prmpt.reset_index(drop=True)
+        #sideband
+        side=df.loc[df['weight']!=1]
+        side=side.reset_index(drop=True)
+        nprmpt=len(prmpt)
+        total_nside=(len(side))
+        #weights used in the data, including 1--> exterminate 1
+        weights=pd.unique(df['weight'].values)
+        weights=np.array(weights[weights!=1])
+        #sideband events are identified by the corresponding weights
+        nside=np.array([len(df.loc[df['weight']==weights[i]])for i in range(len(weights))])
+        #fraction of signal in prompt peak, note that the weights are already negative (TO BE CHECKED W/ FARAHS DATA!)
+        f=(nprmpt+np.sum(nside*weights))/nprmpt
+        print(f)
+        stan_data={
+            'N':nprmpt, #no. of prompt peak events and corresponding pol and phi values
+            'phi_prmpt':list(prmpt['phi'].values),
+            'pol_prmpt':list(prmpt['pol'].values),
+            'M':total_nside,#no. of sideband events and..
+            'phi_side':list(side['phi'].values),
+            'pol_side':list(side['pol'].values),
+            'f':f #fraction of signal in prmpt peak
+        }
+        print(nprmpt, total_nside)
+        #now the stan model and mcmc
+        model=sp.CmdStanModel(stan_file='toyMC_stan.stan')
+        model.compile()
+        fitobj=model.sample(data=stan_data,iter_sampling=nsamples,inits=0)
+        summary=fitobj.summary()
+        samples=fitobj.draws_pd()
+        #get mcmc diagnostics
+        median=summary['50%']['sigma']
+        mcse=(az.mcse(np.transpose(fitobj.draws(concat_chains=False)[:,:,7]),method='median'))
+        rhat=(summary['R_hat']['sigma'])
+        tmp_list=[median,mcse,rhat]
+        currbin=f"toybin{i:02d}"
+        diagnostics_df[currbin]=tmp_list
+        sigma_df[currbin]=samples['sigma']
+    return diagnostics_df, sigma_df, summary
+
+dfs=fit(nsamples=1000,nbins=100)
+diagnostics=dfs[0]
+sigma=dfs[1]
+diagnostics.to_csv('toy_diagnostics.csv')
+sigma.to_csv('toy_sigma.csv')
--- bayes/event_based_fit/toy_MC.C
+++ bayes/event_based_fit/toy_MC.C
@@ -1,7 +1,7 @@
-class pdfObject{
+class pdfObjectbot{
     public:
     Double_t m_p_gamma;
-    pdfObject(Double_t p_gamma){
+    pdfObjectbot(Double_t p_gamma){
             m_p_gamma=p_gamma;
     }    
     Double_t operator()(Double_t *x, Double_t *p){
@@ -13,7 +13,7 @@ class pdfObject{
             a[i]=p[i+1];
             b[i]=p[i+6];
         }
-        Double_t nmrtr=1-1./2.*a[2]*m_p_gamma*sigma;
+        Double_t nmrtr=1-0.5*a[2]*m_p_gamma*sigma;
         Double_t enmrtr=1+m_p_gamma*sigma*TMath::Cos(2*(-45-x[0])*TMath::Pi()/180.);
         Double_t acc_sum=0;
         for(int i=0;i<=4;i++){
@@ -25,118 +25,63 @@ class pdfObject{
 
  
 };
+class pdfObjectpar{
+    public:
+    Double_t m_p_gamma;
+    pdfObjectpar(Double_t p_gamma){
+            m_p_gamma=p_gamma;
+    }    
+    Double_t operator()(Double_t *x, Double_t *p){
+        Double_t sigma=p[0]; //beam asymmetry
+        //these are the 10 acceptance coefficients
+        Double_t a[5];
+        Double_t b[5];
+        for(int i=0;i<=4;i++){
+            a[i]=p[i+1];
+            b[i]=p[i+6];
+        }
+        Double_t nmrtr=1+0.5*a[2]*m_p_gamma*sigma;
+        Double_t enmrtr=1-m_p_gamma*sigma*TMath::Cos(2*(-45-x[0])*TMath::Pi()/180.);
+        Double_t acc_sum=0;
+        for(int i=0;i<=4;i++){
+           acc_sum+=a[i]*TMath::Sin(i*x[0]*TMath::Pi()/180.)+b[i]*TMath::Cos(i*x[0]*TMath::Pi()/180.);
+        }
+        enmrtr*=acc_sum;
+        return enmrtr;    
+    }
+
+ 
+};
 
 
 
 void toy_MC(){
 //std::cout<<"hello world"<<std::endl;
 //get pdf for parallel configuration, thus take neg. value
-pdfObject n_par(-0.25);
-pdfObject n_bot(0.3);
-//TF1* test = new TF1("test","(1-0.25*0.5*TMath::Cos(2*(-45-x)*TMath::Pi()/180))*1./10.5*(9.3+0.28*TMath::Cos(x*TMath::Pi()/180.)+0.24*TMath::Sin(3*x*TMath::Pi()/180.))",-180,180);
-//test->SetLineColor(kBlue);
-//test->SetLineStyle(kDashed);
+pdfObjectpar n_par(0.25);
+pdfObjectbot n_bot(0.3);
+
 TF1* par_sig = new TF1("par_sig",n_par,-180,180,11);
-//sigma
-par_sig->SetParameter(0,0.5);
-//a0
-par_sig->SetParameter(1,0);
-//a1
-par_sig->SetParameter(2,0);
-//a2
-par_sig->SetParameter(3,0);
-//a3
-par_sig->SetParameter(4,0.24/10.5);
-//par_sig->SetParameter(4,0);
-//a4
-par_sig->SetParameter(5,0);
-//b0
-par_sig->SetParameter(6,9.3/10.5);
-//par_sig->SetParameter(6,1);
-//b1
-par_sig->SetParameter(7,0.28/10.5);
-//par_sig->SetParameter(7,0);
-//b2
-par_sig->SetParameter(8,0);
-//b3
-par_sig->SetParameter(9,0);
-//b4
-par_sig->SetParameter(10,0);
-
 TF1* par_bkg = new TF1("par_bkg",n_par,-180,180,11);
-//sigma bkg
-par_bkg->SetParameter(0,-0.5);
-//a0
-par_bkg->SetParameter(1,0);
-//a1
-par_bkg->SetParameter(2,0);
-//a2
-par_bkg->SetParameter(3,0);
-//a3
-par_bkg->SetParameter(4,0.24/10.5);
-//a4
-par_bkg->SetParameter(5,0);
-//b0
-par_bkg->SetParameter(6,9.3/10.5);
-//b1
-par_bkg->SetParameter(7,0.28/10.5);
-//b2
-par_bkg->SetParameter(8,0);
-//b3
-par_bkg->SetParameter(9,0);
-//b4
-par_bkg->SetParameter(10,0);
-
 TF1* bot_sig = new TF1("bot_sig",n_bot,-180,180,11);
-//sigma
-bot_sig->SetParameter(0,0.5);
-//a0
-bot_sig->SetParameter(1,0);
-//a1
-bot_sig->SetParameter(2,0);
-//a2
-bot_sig->SetParameter(3,0);
-//a3
-bot_sig->SetParameter(4,0.24/10.5);
-//bot_sig->SetParameter(4,0);
-//a4
-bot_sig->SetParameter(5,0);
-//b0
-bot_sig->SetParameter(6,9.3/10.5);
-//bot_sig->SetParameter(6,1);
-//b1
-bot_sig->SetParameter(7,0.28/10.5);
-//bot_sig->SetParameter(7,0);
-//b2
-bot_sig->SetParameter(8,0);
-//b3
-bot_sig->SetParameter(9,0);
-//b4
-bot_sig->SetParameter(10,0);
-
 TF1* bot_bkg = new TF1("bot_bkg",n_bot,-180,180,11);
-//sigma bkg
-bot_bkg->SetParameter(0,-0.5);
-//a0
-bot_bkg->SetParameter(1,0);
-//a1
-bot_bkg->SetParameter(2,0);
-//a2
-bot_bkg->SetParameter(3,0);
-//a3
-bot_bkg->SetParameter(4,0.24/10.5);
-//a4
-bot_bkg->SetParameter(5,0);
-//b0
-bot_bkg->SetParameter(6,9.3/10.5);
-//b1
-bot_bkg->SetParameter(7,0.28/10.5);
-//b2
-bot_bkg->SetParameter(8,0);
-//b3
-bot_bkg->SetParameter(9,0);
-//b4
-bot_bkg->SetParameter(10,0);
+
+//order of params: sigma,a0-a4,b0-b4
+double sig_params[11]={0.5,0,0,0,0.24/10.5,0,9.3/10.5,0.28/10.5,0,0,0};
+//double sig_params[11]={0.5,0,0,0,0,0,1,0,0,0,0};
+double bkg_params[11]={-0.5,0,0,0,0.24/10.5,0,9.3/10.5,0.28/10.5,0,0,0};
+
+for(int i=0;i<11;i++){
+    par_sig->SetParameter(i,sig_params[i]);
+    par_bkg->SetParameter(i,bkg_params[i]);
+    bot_sig->SetParameter(i,sig_params[i]);
+    bot_bkg->SetParameter(i,bkg_params[i]);
+}
+for(int i=0;i<11;i++){
+    std::cout<<par_sig->GetParameter(i)<<std::endl;
+}
+
+
 
 TRandom3* random=new TRandom3(0);
 //no. of events is poisson distributed
@@ -148,56 +93,66 @@ int n_sig_bot = std::round(0.95*events_bot);
 int n_bkg_bot=events_bot-n_sig_bot;
 int n_sig_par = std::round(0.95*events_par);
 int n_bkg_par=events_par-n_sig_par;
+int n_sideband_bot=20*n_bkg_bot;
+int n_sideband_par=20*n_bkg_par;
 //sideband events
-double weights[7]={15./210.,8./210.,4./210.,10./210.,14./210.,6./210.,11./210.};
+/*double weights[7]={15./210.,8./210.,4./210.,10./210.,14./210.,6./210.,11./210.};
 int n_sideband_bot[7];
 int n_sideband_par[7];
 for(int i=0;i<7;i++){
     n_sideband_par[i]=std::round(1./weights[i]*n_bkg_par/7.);
     n_sideband_bot[i]=std::round(1./weights[i]*n_bkg_bot/7.);
-}
+}*/
 ofstream myfile;
-myfile.open ("test.txt");
+myfile.open ("./toybins/toybin01.txt");
 myfile << "pol.\tphi\tweight\n";
 
 TH1D* hnbot=new TH1D("hnbot",";phi;n_bot",24,-180,180);
 TH1D* hnpar=new TH1D("hnpar",";phi;n_bot",24,-180,180);
 TH1D* hn=new TH1D("hn",";phi;n",24,-180,180);
+TF1* eff = new TF1("f1","[0]*1./10.5*(9.3+0.28*cos(TMath::Pi()/180.*x)+0.24*sin(TMath::Pi()/180.*3*x))",-180,180);
 std::cout<<events_bot<<" "<<events_par<<std::endl;
-
-//generate signal events for bot config.
+//signal events for bot config
 for(int i=0;i<n_sig_bot;i++){
-    double phi_0=bot_sig->GetRandom();
-    myfile<<n_bot.m_p_gamma<<"\t"<<phi_0<<"\t"<<1<<"\n";
-    hnbot->Fill(phi_0);
-    hn->Fill(phi_0);
+    myfile<<n_bot.m_p_gamma<<"\t"<<bot_sig->GetRandom()<<"\t"<<1<<"\n";
 }
-//generate bkg events for bot config
+//bkg events for bot config
 for(int i=0;i<n_bkg_bot;i++){
     myfile<<n_bot.m_p_gamma<<"\t"<<bot_bkg->GetRandom()<<"\t"<<1<<"\n";
 }
-//generate seven times sideband events for bot config
-for(int i=0;i<7;i++){
-    for(int j=0;j<n_sideband_bot[i];j++){
-        myfile<<n_bot.m_p_gamma<<"\t"<<bot_bkg->GetRandom()<<"\t"<<-1*weights[i]<<"\n";    }
+//sideband events for bot config
+for(int i=0;i<n_sideband_bot;i++){
+    myfile<<n_bot.m_p_gamma<<"\t"<<bot_bkg->GetRandom()<<"\t"<<-1./20.<<"\n";
 }
-//generate signal events for par config.
+
+
+//signal events for par config
 for(int i=0;i<n_sig_par;i++){
-    double phi_1=par_sig->GetRandom();
-    myfile<<n_par.m_p_gamma<<"\t"<<phi_1<<"\t"<<1<<"\n";
-    hnpar->Fill(phi_1);
-    hn->Fill(phi_1);
+    myfile<<n_par.m_p_gamma<<"\t"<<par_sig->GetRandom()<<"\t"<<1<<"\n";
 }
-//generate bkg events for par config
+//bkg events for bot config
 for(int i=0;i<n_bkg_par;i++){
     myfile<<n_par.m_p_gamma<<"\t"<<par_bkg->GetRandom()<<"\t"<<1<<"\n";
 }
-//generate seven times sideband events for par config
-for(int i=0;i<7;i++){
-    for(int j=0;j<n_sideband_par[i];j++){
-    myfile<<n_par.m_p_gamma<<"\t"<<par_bkg->GetRandom()<<"\t"<<-1*weights[i]<<"\n";    
-    }
+//sideband events for bot config
+for(int i=0;i<n_sideband_par;i++){
+    myfile<<n_par.m_p_gamma<<"\t"<<par_bkg->GetRandom()<<"\t"<<-1./20.<<"\n";
 }
+
+
+
+
+
+double scale_bot=1./hnbot->Integral();
+double scale_par=1./hnpar->Integral();
+//hnbot->Scale(scale_bot);
+//hnpar->Scale(scale_par);
+
+
+//hnbot->Add(hnpar);
+//hnbot->Draw("pe");
+//hnbot->Fit("f1");
+
 
 
 }
