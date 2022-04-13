e348d1e2f6ad5fb8832454c695d748b24960803e
Jakob Krause <krause.jakobmichael@gmail.com>
2022-04-13 08:47:06 +0200

    pi0 toyMC

--- bayes/toyMC/new_toy_MC.C
+++ bayes/toyMC/new_toy_MC.C
@@ -34,7 +34,8 @@ void make_bin(int k){
 TF1* mypdf=new TF1("mypdf",pdf,-180,180,3);
 //this is the file data will be written to
 ofstream myfile;
-myfile.open (Form("./toybins/toybin%04d.txt",k));
+//myfile.open (Form("./toybins/toybin%04d.txt",k));
+myfile.open (Form("./pi0_toybins/toybin%04d.txt",k));
 myfile << "pol\tsetting\tphi\n";
 
 TRandom3 r(0);
@@ -42,8 +43,8 @@ gRandom->SetSeed(0);
 double p45pol=0.300000;
 double m45pol=0.250000;
 double sigma=0.30000;
-int n1=1000;
-int n2=800;
+int n1=5000;
+int n2=4000;
 cout<<Form("throwing toy MC experiment no.%d",k)<<endl;
 cout<<n1<<endl;
 cout<<n2<<endl;
@@ -59,11 +60,11 @@ for(int i=0;i<n2;i++){
     mypdf->FixParameter(0,sigma);//sigma
     mypdf->FixParameter(1,m45pol);//pol
     mypdf->FixParameter(2,-45);//setting
-    myfile<<m45pol<<"\t"45<<"\t"<<mypdf->GetRandom()<<"\n";
+    myfile<<m45pol<<"\t"<<-45<<"\t"<<mypdf->GetRandom()<<"\n";
 }
 }
 void new_toy_MC(){
-    for(int k=0;k<1000;k++){
+    for(int k=0;k<10000;k++){
         make_bin(k);
     }
 
