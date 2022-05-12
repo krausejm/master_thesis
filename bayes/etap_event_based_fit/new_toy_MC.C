4aaa57201486c90f68def868241accf88ae979e9
jakob <krause.jakobmichael@gmail.com>
2022-05-12 14:07:25 +0200

    create new toybins for etap

--- bayes/etap_event_based_fit/new_toy_MC.C
+++ bayes/etap_event_based_fit/new_toy_MC.C
@@ -41,66 +41,66 @@ myfile << "pol.\tphi\tweight\n";
 TRandom3 r(0);
 gRandom->SetSeed(0);
 double p45pol=0.300000;
-double m45pol=0.250000;
+double m45pol=0.300000;
 double sigma=0.5;
 double sigma_bkg=-0.5;
-double weights[7]={15./210.,8./210.,4./210.,10./210.,14./210.,6./210.,11./210.};
-int n1=r.Poisson(1000);//statistics of final state, n1=p45, n2=m45
-int n2=r.Poisson(800);
+double weight=0.065;
+int n1=r.Poisson(200);//statistics of final state, n1=p45, n2=m45
+int n2=r.Poisson(200);
 cout<<Form("throwing toy MC experiment no.%d",k)<<endl;
 cout<<n1<<endl;
 cout<<n2<<endl;
 double f = 0.95; // fraction of signal events in prmpt peak
 
-for(int i=0;i<7;i++){// 1 iteration for each time cut
+for(int i=0;i<1;i++){// 1 iteration for each time cut
     //signal events for first setting
-    for(int j=0;j<int(n1/7.*f);j++){
+    for(int j=0;j<int(n1*f);j++){
             mypdf->FixParameter(0,sigma);//sigma
             mypdf->FixParameter(1,p45pol);//pol
             mypdf->FixParameter(2,+45);//setting
             myfile<<p45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<1<<"\n";
     }
     //bkg events for first setting
-    for(int j=0;j<int(n1/7.*(1-f));j++){
+    for(int j=0;j<int(n1*(1-f));j++){
             mypdf->FixParameter(0,sigma_bkg);//sigma bkg
             mypdf->FixParameter(1,p45pol);//pol
             mypdf->FixParameter(2,+45);//setting
             myfile<<p45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<1<<"\n";
     }
     //sideband events for first setting
-    for(int j=0;j<int(n1/7.*(1-f)*1./weights[i]);j++){
+    for(int j=0;j<int(n1*(1-f)*1./weight);j++){
             mypdf->FixParameter(0,sigma_bkg);//sigma bkg
             mypdf->FixParameter(1,p45pol);//pol
             mypdf->FixParameter(2,+45);//setting
-            myfile<<p45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<-1*weights[i]<<"\n";
+            myfile<<p45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<-1*weight<<"\n";
     }
     ////////////////////////////////////////////////////////////////////////////
     //signal events for second setting
-    for(int j=0;j<int(n2/7.*f);j++){
+    for(int j=0;j<int(n2*f);j++){
             mypdf->FixParameter(0,sigma);//sigma
             mypdf->FixParameter(1,m45pol);//pol
             mypdf->FixParameter(2,-45);//setting
             myfile<<-1*m45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<1<<"\n";
     }
     //bkg events for first setting
-    for(int j=0;j<int(n2/7.*(1-f));j++){
+    for(int j=0;j<int(n2*(1-f));j++){
             mypdf->FixParameter(0,sigma_bkg);//sigma bkg
             mypdf->FixParameter(1,m45pol);//pol
             mypdf->FixParameter(2,-45);//setting
             myfile<<-1*m45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<1<<"\n";
     }
     //sideband events for first setting
-    for(int j=0;j<int(n2/7.*(1-f)*1./weights[i]);j++){
+    for(int j=0;j<int(n2*(1-f)*1./weight);j++){
             mypdf->FixParameter(0,sigma_bkg);//sigma bkg
             mypdf->FixParameter(1,m45pol);//pol
             mypdf->FixParameter(2,-45);//setting
-            myfile<<-1*m45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<-1*weights[i]<<"\n";
+            myfile<<-1*m45pol<<"\t"<<mypdf->GetRandom()<<"\t"<<-1*weight<<"\n";
     }
 }
 
 }
 void new_toy_MC(){
-    for(int k=0+200;k<200+100;k++){
+    for(int k=0;k<1000;k++){
         make_bin(k);
     }
 
---
+++ bayes/etap_event_based_fit/toyMC_stan.stan
@@ -0,0 +1,79 @@
+functions{
+
+real mypdf(real phi, real pol, real sigma,vector a, vector b){
+    vector[5] m_a;
+    vector[5] m_b;
+    //the first coefficients are set!
+    m_a[1]=0;
+    m_b[1]=1;
+    //for convenience make vectors of length 5 for easy use in loops
+    for(k in 1:4){
+        m_a[k+1]=a[k];
+        m_b[k+1]=b[k];
+    }
+    //enumerator and numerator of pdf
+    real enmrtr = 1-0.5*m_a[3]*pol*sigma;
+    real nmrtr = 1+pol*sigma*cos(2*(-45-phi)*pi()/180.);
+    //have to multiply numerator by efficiency function
+    real angle=phi*pi()/180.;
+    vector[5] sines;
+    vector[5] cosines;
+    for(k in 0:4){
+        sines[k+1]=sin(k*angle);
+        cosines[k+1]=cos(k*angle);
+    }
+    real nnmrtr=nmrtr*(dot_product(sines,m_a)+dot_product(cosines,m_b));
+    return nnmrtr/enmrtr;
+}
+}
+data {
+    //prmpt peak data
+	int<lower=0> N;
+	vector[N] phi_prmpt;
+	vector[N] pol_prmpt;
+    //sideband data
+    int<lower=0> M;
+    vector[M] phi_side;
+    vector[M] pol_side;
+    //fraction of bkg events in prmpt peak
+    real<lower=0,upper=1> f;
+}
+parameters {
+    //to do: dont use vector??
+    //18 pars in total
+
+    //sigma and detector coefficients for signal events
+	real<lower=-1, upper=1> sigma;
+	vector[4] a;
+
+    vector[4] b;
+
+    //sigma and detector coefficients for bkg events
+    real<lower=-1, upper=1> sigma_bkg;
+	vector[4] a_bkg;
+ 
+    vector[4] b_bkg;
+
+}
+model{
+
+//loop over prmpt peak events
+for(k in 1:N){
+    target+=log(f*mypdf(phi_prmpt[k],pol_prmpt[k],sigma,a,b)+(1-f)*mypdf(phi_prmpt[k],pol_prmpt[k],sigma_bkg,a_bkg,b_bkg));
+}
+//loop over sideband events
+for(k in 1:M){
+    target+=log(mypdf(phi_side[k],pol_side[k],sigma_bkg,a_bkg,b_bkg));
+}
+
+//priors for eff. coefficients, non-informative, broadly around 0
+for(k in 1:4){
+    a[k]~normal(0,0.1);
+    b[k]~normal(0,0.1);
+    a_bkg[k]~normal(0,0.1);
+    b_bkg[k]~normal(0,0.1);
+}
+//priors for sigma
+sigma ~ normal(0,1) T[-1,1];
+sigma_bkg ~ normal(0,1) T[-1,1];
+}
